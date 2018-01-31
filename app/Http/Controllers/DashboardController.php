58c62a9ac999a9ff3ef906e1fc9cf01a1810c29a
Christopher Yovanovitch <yovano_c@outlook.com>
2018-01-31 23:01:10 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view("dashboard");
+        return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 
-/**
+/*
  * Account
  */
 Route::group(['prefix' => 'account', 'middleware' => ['auth'], 'as' => 'account.'], function () {
     Route::get('/', 'Account\AccountController@index')->name('index');
-    /**
+    /*
      * Profile
      */
     Route::get('/profile', 'Account\ProfileController@index')->name('profile.index');
