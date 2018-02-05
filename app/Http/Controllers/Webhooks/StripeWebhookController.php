3fdfe777e9100b2e78143107c6e741ccf569d908
Christopher Yovanovitch <yovano_c@outlook.com>
2018-02-05 16:36:00 +0100

    Stripe Webhooks

---
+++ app/Http/Controllers/Webhooks/StripeWebhookController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Webhooks;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Cashier\Http\Controllers\WebhookController;
+
+class StripeWebhookController extends WebhookController
+{
+    //
+}
