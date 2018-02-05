bc4e2fa8bea13cc68559c633a702aefa50650a41
Christopher Yovanovitch <yovano_c@outlook.com>
2018-02-05 15:36:11 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Webhooks/StripeWebhookController.php
+++ app/Http/Controllers/Webhooks/StripeWebhookController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Webhooks;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Laravel\Cashier\Http\Controllers\WebhookController;
 
 class StripeWebhookController extends WebhookController
